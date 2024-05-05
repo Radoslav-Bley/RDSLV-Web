ffcb63ff4361066d2d0a36fab5a3adb2cb6a4123
radblesk <144258807+radblesk@users.noreply.github.com>
2024-05-05 21:44:45 +0200

    fixed closing on click
    After clicking on link from menu, menu closed but wasn't possible to navigate page. Now fixed

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -5,10 +5,23 @@
   var burgerMenu = document.getElementsByClassName('b-menu')[0];
   var burgerContain = document.getElementsByClassName('b-container')[0];
   var burgerNav = document.getElementsByClassName('b-nav')[0];
+  var links = document.querySelectorAll('.b-nav a');
 
-  burgerMenu.addEventListener('click', function toggleClasses() {
+  function toggleClasses() {
     [body, burgerContain, burgerNav].forEach(function (el) {
       el.classList.toggle('open');
     });
-  }, false);
+  }
+
+  burgerMenu.addEventListener('click', toggleClasses, false);
+
+  // Add event listener to links to remove 'open' class
+  links.forEach(function(link) {
+    link.addEventListener('click', function() {
+      [body, burgerContain, burgerNav].forEach(function (el) {
+        el.classList.remove('open');
+      });
+    });
+  });
 })();
+
