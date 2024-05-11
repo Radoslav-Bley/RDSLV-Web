ffbdc935575a36fd6c655d884020b8d9607843db
radblesk <144258807+radblesk@users.noreply.github.com>
2024-05-11 19:39:19 +0200

    #20 #7

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,28 @@
-var menuContainer = document.querySelector('#menu-toggle');
-var menuTextOpen = document.querySelector('#menu-text-open');
-var menuTextClose = document.querySelector('#menu-text-close');
+var hamburgerMenu = document.querySelector('.hamburger-menu');
 var fullscreenMenu = document.querySelector('#fullscreen-menu');
-var body = document.body; // Add this line
+var menuItems = document.querySelectorAll('#fullscreen-menu .mobile-nav ul li');
+var socialIcons = document.querySelectorAll('.rounded-social-buttons a');
+var body = document.body;
 
-menuContainer.addEventListener('click', function() {
-  var hamburger = document.querySelector('.hamburger');
-  if (menuTextOpen.style.display !== 'none') {
-    menuTextOpen.style.display = 'none';
-    menuTextClose.style.display = 'block';
-    fullscreenMenu.style.display = 'flex';
-    body.style.overflow = 'hidden'; // Add this line
+hamburgerMenu.addEventListener('click', function() {
+  this.classList.toggle('active');
+  fullscreenMenu.classList.toggle('active');
+
+  if (fullscreenMenu.classList.contains('active')) {
+    body.style.overflow = 'hidden';
+    menuItems.forEach(function(item, index) {
+      item.style.animation = `slideInBlurredTop 0.5s cubic-bezier(0.23, 1, 0.32, 1) ${index * 0.1}s both`;
+    });
+    socialIcons.forEach(function(icon, index) {
+      icon.style.animation = `slideInBlurredTop 0.5s cubic-bezier(0.23, 1, 0.32, 1) ${(menuItems.length + index) * 0.2}s both`;
+    });
   } else {
-    menuTextOpen.style.display = 'block';
-    menuTextClose.style.display = 'none';
-    fullscreenMenu.style.display = 'none';
-    body.style.overflow = 'auto'; // Add this line
+    body.style.overflow = 'auto';
+    menuItems.forEach(function(item) {
+      item.style.animation = '';
+    });
+    socialIcons.forEach(function(icon) {
+      icon.style.animation = '';
+    });
   }
-  hamburger.classList.toggle('active');
 });
--- scripts/script.js
+++ scripts/script.js
@@ -115,3 +115,12 @@ function loadYoutubeVideo() {
   document.getElementById("youtube-video").innerHTML =
     '<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
+
+/*!========================================================================
+ * 05. Header background on scroll
+ * ======================================================================!*/
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
