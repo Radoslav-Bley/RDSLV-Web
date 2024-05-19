f63d570cc2e922a43b3375e497cdc415223f4cb1
radblesk <144258807+radblesk@users.noreply.github.com>
2024-05-20 00:27:32 +0200

    Big rework
    - Removed unused/unnecessary/obsolete files
    
    - Completely reworked stylesheets, easier on performance, reorganized, deleted unused code, reworked paddings and margins, font sizes
    
    - Implemented menu.css into global stylesheet
    
    - Completely reworked responsiveness for all media queries, added safe area for iOS, added portrait and landscape queries
    
    - Reworked dark mode
    
    - Reworked and unified animations
    
    - Reworked html structure
    
    - Reworked and updated privacy policy page
    
    - Unified JavaScripts
    
    Summary:
    Biggest design and performance update since start, reworked containers, structures, unified scripts, stylesheets, removed obsolete files

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,7 @@
  * 04. YouTube Lazy Loading
  * 05. Header background on scroll
  * 06. Grid Drawing Animation
+ * 07. Menu Toggle
  */
 
 /*!========================================================================
@@ -153,4 +154,51 @@ document.addEventListener('DOMContentLoaded', () => {
     line.style.animationDelay = `${i * 0.1}s`;
     horizontalLinesContainer.appendChild(line);
   }
+});
+
+/*!========================================================================
+ * 07. Menu Toggle
+ * ======================================================================!*/
+
+var hamburgerMenu = document.querySelector('.hamburger-menu');
+var fullscreenMenu = document.querySelector('#fullscreen-menu');
+var menuItems = document.querySelectorAll('#fullscreen-menu .mobile-nav ul li');
+var socialIcons = document.querySelectorAll('.rounded-social-buttons a');
+var body = document.body;
+
+hamburgerMenu.addEventListener('click', function() {
+  this.classList.toggle('active');
+  fullscreenMenu.classList.toggle('active');
+
+  if (fullscreenMenu.classList.contains('active')) {
+    body.style.overflow = 'hidden';
+    menuItems.forEach(function(item, index) {
+      item.style.animation = `slideInBlurredTop 0.5s cubic-bezier(0.23, 1, 0.32, 1) ${index * 0.1}s both`;
+    });
+    socialIcons.forEach(function(icon, index) {
+      icon.style.animation = `slideInBlurredTop 0.5s cubic-bezier(0.23, 1, 0.32, 1) ${(menuItems.length + index) * 0.2}s both`;
+    });
+  } else {
+    body.style.overflow = 'auto';
+    menuItems.forEach(function(item) {
+      item.style.animation = '';
+    });
+    socialIcons.forEach(function(icon) {
+      icon.style.animation = '';
+    });
+  }
+});
+
+menuItems.forEach(function(item) {
+  item.addEventListener('click', function() {
+    hamburgerMenu.classList.remove('active');
+    fullscreenMenu.classList.remove('active');
+    body.style.overflow = 'auto';
+    menuItems.forEach(function(item) {
+      item.style.animation = '';
+    });
+    socialIcons.forEach(function(icon) {
+      icon.style.animation = '';
+    });
+  });
 });
