f5f07d5ceeb58f320d49065c0f5bd1c2c9d6d185
Radoslav Bley <144258807+radblesk@users.noreply.github.com>
2024-06-20 00:20:23 +0200

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -51,12 +51,12 @@ function setAddressBarColor() {
     // Dark mode
     document
       .querySelector('meta[name="theme-color"]')
-      .setAttribute("content", "#000000");
+      .setAttribute("content", "rgb(20, 20, 20)");
   } else {
     // Light mode
     document
       .querySelector('meta[name="theme-color"]')
-      .setAttribute("content", "#ffffff");
+      .setAttribute("content", "rgb(244, 248, 255)");
   }
 }
 
@@ -273,45 +273,74 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 /*!========================================================================
- * 09. Swiper Slider
- * ======================================================================!*/
+* 08. Blob Animation
+* ======================================================================!*/
 
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 'auto', // Automatically adjusts to the size of the slides
-  spaceBetween: 30,
-  centeredSlides: true, // Center the active slide
-  centeredSlidesBounds: true, // Ensure the active slide is fully visible
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  on: {
-    init: function () {
-      // Hide the previous and next buttons initially
-      document.querySelector('.swiper-button-prev').style.display = 'none';
-      document.querySelector('.swiper-button-next').style.display = 'block'; // Adjust if you want to hide initially
-    },
-    slideChange: function () {
-      // Show the previous button after the first slide change
-      if (swiper.activeIndex > 0) {
-        document.querySelector('.swiper-button-prev').style.display = 'block';
-      } else {
-        document.querySelector('.swiper-button-prev').style.display = 'none';
-      }
+const MIN_SPEED = 1.5
+const MAX_SPEED = 2.5
 
-      // Hide the next button when the last slide is reached
-      if (swiper.activeIndex === swiper.slides.length - 1) {
-        document.querySelector('.swiper-button-next').style.display = 'none';
-      } else {
-        document.querySelector('.swiper-button-next').style.display = 'block';
-      }
-    },
-  },
-});
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min
+}
 
+class Blob {
+  constructor(el) {
+    this.el = el
+    const boundingRect = this.el.getBoundingClientRect()
+    this.size = boundingRect.width
+    this.initialX = randomNumber(0, window.innerWidth - this.size)
+    this.initialY = randomNumber(0, window.innerHeight - this.size)
+    this.el.style.top = `${this.initialY}px`
+    this.el.style.left = `${this.initialX}px`
+    this.vx =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.vy =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.x = this.initialX
+    this.y = this.initialY
+  }
 
-/*!========================================================================
- * 10. Theme Switcher
- * ======================================================================!*/
+  update() {
+    this.x += this.vx
+    this.y += this.vy
+    if (this.x >= window.innerWidth - this.size) {
+      this.x = window.innerWidth - this.size
+      this.vx *= -1
+    }
+    if (this.y >= window.innerHeight - this.size) {
+      this.y = window.innerHeight - this.size
+      this.vy *= -1
+    }
+    if (this.x <= 0) {
+      this.x = 0
+      this.vx *= -1
+    }
+    if (this.y <= 0) {
+      this.y = 0
+      this.vy *= -1
+    }
+  }
 
+  move() {
+    this.el.style.transform = `translate(${this.x - this.initialX}px, ${
+      this.y - this.initialY
+    }px)`
+  }
+}
 
+function initBlobs() {
+  const blobEls = document.querySelectorAll('.bouncing-blob')
+  const blobs = Array.from(blobEls).map((blobEl) => new Blob(blobEl))
+
+  function update() {
+    requestAnimationFrame(update)
+    blobs.forEach((blob) => {
+      blob.update()
+      blob.move()
+    })
+  }
+
+  requestAnimationFrame(update)
+}
+
+initBlobs()
